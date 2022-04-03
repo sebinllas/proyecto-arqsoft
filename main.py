6b5f251e9ce299b88d538f6f9d51cf20e9320a35
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2022-04-03 00:03:46 -0500

    Configuración de CORS

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from routes import enrollments
 from routes import auth
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='matriculas API', docs_url='/')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(enrollments.router)

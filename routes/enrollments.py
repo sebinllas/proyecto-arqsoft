cfb124528d13ae13621b6781b21be2474691727b
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2022-04-04 12:08:23 -0500

    arreglado update

--- routes/enrollments.py
+++ routes/enrollments.py
@@ -39,7 +39,12 @@ def new_enrollment(enrollment: Enrollemnt):
     new_enrollment = enrollment.dict(by_alias=True)
     student = enrollment.student.dict(by_alias=True)
     new_enrollment['student'] = {'_id': enrollment.student.id, }
-    students.insert_one(student)
+    old_student = students.find_one({'_id': enrollment.student.id})
+    if old_student:
+        students.update_one({'_id': enrollment.student.id}, {
+                            '$set': {**student}})
+    else:
+        students.insert_one(student)
     enrollments.insert_one(new_enrollment)
     return 'saved'
 
@@ -49,12 +54,20 @@ def update_enrollment(id: str, new_enrollment: UpdateEnrollment):
     new_data_enrollment = new_enrollment.dict(exclude_unset=True)
     modified_count = 0
     enrollment = enrollments.find_one({'_id': id})
+    print('enrollment antiguo', enrollment)
+    print('std ID: ', enrollment['student']['_id'])
+
     if not enrollment:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
-
-    if new_data_enrollment['student']:
+    student = students.find_one({'_id': enrollment['student']['_id']})
+    print('student antiguo: ', student)
+    if 'student' in new_data_enrollment.keys():
         new_data_student = new_data_enrollment.pop('student')
+        if 'attendant' in new_data_student.keys():
+            new_data_attendant = student['attendant']
+            new_data_attendant.update(new_data_student['attendant'])
+            new_data_student['attendant'] = new_data_attendant
         print(new_data_student)
         result_student = students.update_one({'_id': enrollment['student']['_id']}, {
                                              '$set': {**new_data_student}})

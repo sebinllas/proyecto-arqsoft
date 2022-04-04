3f9a05e4980ed5f7b6491afae14b2135195d17ef
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2022-04-03 21:00:50 -0500

    cambio formato de fecha

--- models/enrollment.py
+++ models/enrollment.py
@@ -1,10 +1,10 @@
 from uuid import uuid4
 from pydantic import UUID4, BaseModel, Field
 from .student import Student
-from datetime import datetime
+import datetime
 
 
 class Enrollemnt(BaseModel):
     id: UUID4 = Field(alias="_id", default_factory=lambda: str(uuid4()))
-    date: datetime = datetime.now()
+    date: str = 'yyyy-mm-dd'
     student: Student

6b0a0c0e5900671e78644b1e953cd958885abe7d
Sebastián Suárez <sebastian.suarezr@udea.edu.co>
2022-04-07 17:51:51 -0500

    modificando CORS

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from routes import auth
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title='matriculas API', docs_url='/')
-origins = ["http://localhost:4200"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 
